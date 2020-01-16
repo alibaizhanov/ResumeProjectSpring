b0000d7f0528bcc74772671039b34dac10b974d0
Ali Baizhanov <alibaizhanovbcc@gmail.com>
2020-01-17 00:02:49 +0600

    resume project commit commit

--- src/main/java/resume/repository/storage/SkillCategoryRepository2.java
+++ src/main/java/resume/repository/storage/SkillCategoryRepository2.java
@@ -1,9 +1,11 @@
 package resume.repository.storage;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import resume.entity.SkillCategory;
 
 public interface SkillCategoryRepository2 extends PagingAndSortingRepository<SkillCategory,Long> {
 
-
+    @Override
+    Iterable<SkillCategory> findAll(Sort sort);
 }
